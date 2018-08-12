a12c4860b8f9b79f42c60afbc1567dac517cdb25
Arvind <bhusnur@seas.upenn.edu>
2018-08-12 16:03:27 -0400

    created a repository for my regular expression code

---
+++ src/RegexExamples.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+
+public class RegexExamples {
+	public static void main(String[] args) {
+		File f = new File("Countries");
+		Pattern fiveLetter = Pattern.compile("^\\w{5}$") ; 
+		try {
+			Scanner scanner = new Scanner(f);
+			while(scanner.hasNextLine()) {
+				String s = scanner.nextLine();
+				Matcher m = fiveLetter.matcher(s);
+				if ((m.find()) && s.charAt(0) == 'I') {
+					System.out.println(s);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
